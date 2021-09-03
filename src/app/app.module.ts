957bc461655dbf769baca4e78d2804b1138ea6b1
Constance <hytan.2018@sis.smu.edu.sg>
2021-09-04 01:29:37 +0800

    add counter code from ngrx example
    reference: https://ngrx.io/guide/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './modules/counter/store/counter.reducer';
+import { MyCounterComponent } from './modules/counter/components/my-counter/my-counter.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({maxAge: 25})
   ],
   providers: [],
   bootstrap: [AppComponent]

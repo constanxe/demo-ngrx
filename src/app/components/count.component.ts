44347b79375b656ab02331593ff47bd39e53d90f
Constance <hytan.2018@sis.smu.edu.sg>
2021-09-04 20:13:52 +0800

    fix count component pass bg color class to span

--- src/app/components/count.component.ts
+++ src/app/components/count.component.ts
@@ -4,10 +4,11 @@ import { Component, Input } from '@angular/core';
   selector: 'app-count',
   template: `
     <div class="py-2">
-      Current Count: <span class="bg-dark text-white">{{ count }}</span>
+      Current Count: <span class="{{ countClass }} text-white">{{ count }}</span>
     </div>
   `,
 })
 export class CountComponent {
   @Input() count?: number | null;
+  @Input() countClass = 'bg-dark';
 }

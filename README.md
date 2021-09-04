ef613becf9b432bd976228f313461aeabb2260e8
Constance <hytan.2018@sis.smu.edu.sg>
2021-09-04 19:31:02 +0800

    minor cleanups (indentations & README title)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NGRX Demo (Counter)
+# NgRx Demo (Counter)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container my-3">
-    <h1>Counter</h1>
+    <div class="container my-3">
+      <h1>Counter</h1>
 
-    <ul class="nav nav-tabs my-3">
-      <li class="nav-item">
-        <a class="nav-link" routerLink="/basic-counter" routerLinkActive="active">Basic</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" routerLink="/counter-with-selector" routerLinkActive="active">With Selector</a>
-      </li>
-    </ul>
+      <ul class="nav nav-tabs my-3">
+        <li class="nav-item">
+          <a class="nav-link" routerLink="/basic-counter" routerLinkActive="active">Basic</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" routerLink="/counter-with-selector" routerLinkActive="active">With Selector</a>
+        </li>
+      </ul>
 
-    <!-- The routed views render in the <router-outlet>-->
-    <router-outlet></router-outlet>
+      <!-- The routed views render in the <router-outlet>-->
+      <router-outlet></router-outlet>
     </div>
   `,
-  styles: []
 })
 export class AppComponent { }
